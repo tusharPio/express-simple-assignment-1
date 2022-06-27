7f9b15ca44c9ec786ee2aa8248ec590ca2730d1b
tusharPio <tushar.kumar@programmers.io>
2022-06-27 16:55:51 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
-// const express = require('express');
-// const app = express();
-// const port = 3010;
-// const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3010;
+const path = require('path');
 
-// app.use(express.static('static'));
+app.use(express.static('static'));
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve('pages/index.html'));
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });
-
-var express = require('express'); // requre the express framework
-var app = express();
-var fs = require('fs'); //require file system object
-
-// Endpoint to Get a list of users
-app.get('/getUsers', function (req, res) {
-  fs.readFile(__dirname + '/' + 'product.json', 'utf8', function (err, data) {
-    console.log(data);
-    res.end(data); // you can also use res.send()
-  });
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('pages/index.html'));
 });
 
-// Create a server to listen at port 8080
-var server = app.listen(8080, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
