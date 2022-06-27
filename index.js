c314aeb187f187146a4c69af86b24c32b3ff8f22
tusharPio <tushar.kumar@programmers.io>
2022-06-27 16:45:21 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-const express = require('express');
-const app = express();
-const port = 3010;
-const path = require('path');
+// const express = require('express');
+// const app = express();
+// const port = 3010;
+// const path = require('path');
 
-app.use(express.static('static'));
+// app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('pages/index.html'));
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve('pages/index.html'));
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+var express = require('express'); // requre the express framework
+var app = express();
+var fs = require('fs'); //require file system object
+
+// Endpoint to Get a list of users
+app.get('/getUsers', function (req, res) {
+  fs.readFile(__dirname + '/' + 'product.json', 'utf8', function (err, data) {
+    console.log(data);
+    res.end(data); // you can also use res.send()
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// Create a server to listen at port 8080
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 });
