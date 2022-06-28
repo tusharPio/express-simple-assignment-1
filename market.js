6b3794cc0e61da8fdc413add0461daf85603a512
tusharPio <tushar.kumar@programmers.io>
2022-06-28 16:05:05 +0530

    1 file modified

--- market.js
+++ market.js
@@ -1,16 +1,38 @@
-//REST API demo in Node.js
+//Eradani Node-js assignment
+
 var express = require('express'); // requre the express framework
 var app = express();
 var fs = require('fs'); //require file system object
 const port = 3010;
+var product =  {
+  "product3": {
+    "id": "3",
+    "name": "CPU",
+    "cost": 1000,
+    "description": "Central Processing Unit"
+  },
+}
 
-// Endpoint to Get a list of products
+
+// GET method
 app.get('/getProducts', function (req, res) {
   fs.readFile(__dirname + '/' + 'product.json', 'utf8', function (err, data) {
     console.log(data);
-    res.end(data); // you can also use res.send()
+    res.end(data); 
   });
 });
+
+//POST method
+app.post('/addProduct', function(req, res){
+  //Step 2: read existing users
+  fs.readFile(__dirname + "/" + "product.json", 'utf8', function(err, data){
+      data = JSON.parse(data);
+      //Step 3: append user variable to list
+      data["product3"] = product["product3"];
+      console.log(data);
+      res.end(JSON.stringify(data));
+  });
+})
 
 // Create a server to listen at port 3010
 app.listen(port, () => {
