09ca41fa310478d7ff9b78d6624f2bdc2ad683b5
tusharPio <tushar.kumar@programmers.io>
2022-07-03 07:29:50 +0530

    1 file modified

--- market.js
+++ market.js
@@ -14,23 +14,6 @@ var product = {
   },
 };
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-  );
-
-  next();
-});
-
 //POST method
 app.get('/addProduct', function (req, res) {
   //Step 2: read existing users
@@ -43,8 +26,6 @@ app.get('/addProduct', function (req, res) {
     res.end(JSON.stringify(data));
   });
 });
-
-app.use(helmet());
 
 // GET method
 app.get('/getProducts', function (req, res) {
